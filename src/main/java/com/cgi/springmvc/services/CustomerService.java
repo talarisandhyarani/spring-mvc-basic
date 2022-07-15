f3bd94db69244aab1569f231428f8c41016baebd
AdamG-CGI <108414721+AdamG-CGI@users.noreply.github.com>
2022-07-15 11:15:08 -0400

    added getCustomerById
    see title

--- src/main/java/com/cgi/springmvc/services/CustomerService.java
+++ src/main/java/com/cgi/springmvc/services/CustomerService.java
@@ -8,6 +8,7 @@ import org.modelmapper.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CustomerService {
@@ -24,5 +25,12 @@ public class CustomerService {
         customerRepository.findAll().forEach(customers::add);
         List<CustomerDTO>  customersDTO = modelMapper.map(customers, new TypeToken<List<CustomerDTO>>() {}.getType());
         return customersDTO;
+    }
+
+    public CustomerDTO getCustomerById(long cid){
+        Optional<Customer> customer = customerRepository.findById(cid);
+        CustomerDTO  customerDTO = new CustomerDTO();
+        customer.ifPresent(customerData -> modelMapper.map(customerData, customerDTO));
+        return customerDTO;
     }
 }
