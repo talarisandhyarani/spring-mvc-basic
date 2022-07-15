b631b5b4fd6908a02d350ddc3d2b1668e991eedb
AdamG-CGI <108414721+AdamG-CGI@users.noreply.github.com>
2022-07-15 10:55:57 -0400

    getAllCustomers() add to CustomerService
    see title

--- src/main/java/com/cgi/springmvc/services/CustomerService.java
+++ src/main/java/com/cgi/springmvc/services/CustomerService.java
@@ -1,4 +1,24 @@
 package com.cgi.springmvc.services;
 
+import com.cgi.springmvc.beans.Customer;
+import com.cgi.springmvc.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class CustomerService {
+
+    private CustomerRepository customerRepository;
+
+    public CustomerService(CustomerRepository customerRepository){
+        this.customerRepository = customerRepository;
+    }
+
+    public List<Customer> getAllCustomers(){
+        List<Customer> customers = new ArrayList<Customer>();
+        customerRepository.findAll().forEach(customers::add);
+        return customers;
+    }
 }
